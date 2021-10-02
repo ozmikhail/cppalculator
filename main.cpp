a03d3ef8705afc45d3a6c7477608df4ccc2ad4b0
ozmikhail <237983078+ozmikhail@users.noreply.github.com>
2021-10-02 13:18:24 +0300

    feat(main): thread ans through REPL and add history command

--- main.cpp
+++ main.cpp
@@ -1,52 +1,47 @@
 #include <iostream>
-#include <stdexcept>
 #include <string>
-#include "session.hpp"
-
-static void printHelp() {
-    std::cout << "  hist      — show history\n"
-              << "  !N        — recall and re-evaluate entry N\n"
-              << "  clear     — clear history and ans\n"
-              << "  ans       — last result (usable inside expressions)\n"
-              << "  quit/exit — exit\n";
-}
+#include <utility>
+#include <vector>
+#include "lexer.hpp"
+#include "parser.hpp"
 
 int main() {
-    std::cout << "cppalculator  (type 'help' for commands)\n";
+    std::cout << "cppalculator — type an expression or 'quit'\n";
+    std::cout << "  functions: sin cos tan asin acos atan sinh cosh tanh\n";
+    std::cout << "             sqrt cbrt exp ln log log10 log2\n";
+    std::cout << "             abs ceil floor round deg rad sign\n";
+    std::cout << "             min(x,y)  max(x,y)  pow(x,y)  atan2(y,x)\n";
+    std::cout << "             hypot(x,y)  log(x,base)\n";
+    std::cout << "  constants: pi  e  tau  inf\n";
+    std::cout << "  'ans' recalls the last result; 'history' lists prior results\n";
 
-    Session session;
+    std::vector<std::pair<std::string, double>> history;
+    double ans = 0.0;
     std::string line;
 
     while (true) {
         std::cout << "> ";
         if (!std::getline(std::cin, line)) break;
+        if (line == "quit" || line == "exit") break;
         if (line.empty()) continue;
 
-        if (line == "quit" || line == "exit") break;
-        if (line == "help") { printHelp(); continue; }
-        if (line == "clear") { session.clear(); std::cout << "cleared\n"; continue; }
-        if (line == "hist" || line == "history") { session.printHistory(); continue; }
-
-        if (line.size() >= 2 && line[0] == '!') {
-            std::size_t n = 0;
-            try {
-                n = static_cast<std::size_t>(std::stoul(line.substr(1)));
-            } catch (...) {
-                std::cerr << "error: invalid recall syntax — use !N\n";
-                continue;
+        if (line == "history") {
+            if (history.empty()) {
+                std::cout << "(no history)\n";
+            } else {
+                for (std::size_t i = 0; i < history.size(); ++i)
+                    std::cout << "[" << i + 1 << "] "
+                              << history[i].first << " = " << history[i].second << '\n';
             }
-            const auto& hist = session.history();
-            if (n < 1 || n > hist.size()) {
-                std::cerr << "error: no history entry " << n << '\n';
-                continue;
-            }
-            line = hist[n - 1].expression;
-            std::cout << "  >> " << line << '\n';
+            continue;
         }
 
         try {
-            double result = session.evaluate(line);
+            auto tokens = Lexer(line).tokenize();
+            double result = Parser(std::move(tokens), ans).parse();
             std::cout << "= " << result << '\n';
+            ans = result;
+            history.emplace_back(line, result);
         } catch (const std::exception& e) {
             std::cerr << "error: " << e.what() << '\n';
         }
